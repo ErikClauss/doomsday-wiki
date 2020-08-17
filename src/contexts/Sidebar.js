e2d4a5aabb6437b52547c227d76f6ea89376ee04
Mathieu Marques <mathieumarques78@gmail.com>
2020-08-18 00:17:28 +0200

    Wip

--- src/contexts/Sidebar.js
+++ src/contexts/Sidebar.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React, { useCallback, useState } from 'react';
 
 export const SidebarContext = React.createContext();
+
 export function SidebarProvider({ children }) {
   const [ drawer, setDrawer ] = useState(false);
   const toggleDrawer = useCallback(open => () => {
